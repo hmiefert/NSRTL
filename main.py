46f9917793b041b5a71f92e2b53f7c2a08037e05
Holger Miefert <holger.miefert@gmail.com>
2020-04-13 15:47:55 +0200

    Added AutoStart functionality

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from PyQt5.QtWidgets import QApplication
 from gui.window import MainWindow
+import os
 import sys
+import winreg as reg
 
 
 if __name__ == "__main__":
+    # AutoStart
+    key = reg.OpenKey(reg.HKEY_CURRENT_USER, 'Software\Microsoft\Windows\CurrentVersion\Run', 0, reg.KEY_SET_VALUE)
+    reg.SetValueEx(key, 'NSRTL', 0, reg.REG_SZ, os.getcwd() + '\\NSRTL.exe')
+
+    # APP
     app = QApplication(sys.argv)
     mw = MainWindow()
     mw.close()
