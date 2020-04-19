71aa61d4f674c5d079e0e0aae7a0c202bb506624
Holger Miefert <holger.miefert@gmail.com>
2020-04-19 15:17:14 +0200

    Fixed version in window title bar

--- gui/window.py
+++ gui/window.py
@@ -15,7 +15,7 @@ class MainWindow(QMainWindow):
  
         self.setMinimumSize(QSize(600, 800))
         self.setMaximumSize(QSize(600, 800))
-        self.setWindowTitle("NSRTL [version 0.07")
+        self.setWindowTitle("NSRTL [version 0.11]")
         central_widget = QWidget(self)
         self.setCentralWidget(central_widget)
 
